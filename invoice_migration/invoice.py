63fe1b7c6d2efe8d7b3c305fdc9a2d82de895d48
Sebastien Beau <sebastien.beau@akretion.com>
2013-10-04 18:47:17 +0200

    [IMP] start module for migrating the invoice

---
+++ invoice_migration/invoice.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#   Module for OpenERP 
+#   Copyright (C) 2013 Akretion (http://www.akretion.com).
+#   @author Sébastien BEAU <sebastien.beau@akretion.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+
+class account_invoice(orm.Model):
+    _inherit = 'account.invoice'
+   
+    def _get_invoice_number(self, cr, uid, ids, field_name, args, context=None):
+        result = {}
+        for invoice in self.browse(cr, uid, ids, context=context):
+            if invoice.migrated:
+                result[invoice.id] = invoice.internal_number
+            else:
+                result[invoice.id] = invoice.move_id.name
+        return result
+
+    _columns = {
+        'migrated': fields.boolean('Migrated Invoice'),
+        'number': fields.function(_get_invoice_number,
+                            string='Number',
+                            type='char',
+                            store=True,
+                            ),
+    }
+
+    #TODO do not generate the move when validating the sale order
+
+
+
