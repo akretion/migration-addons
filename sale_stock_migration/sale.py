ee2e9a919243e199b8c1b1bf5b851bf02353e02e
Florian da Costa <florian.dacosta@akretion.com.br>
2014-09-16 11:21:51 +0200

    [REF] Do not create picking when flag no_picking is true instread of doing this by date

--- sale_stock_migration/sale.py
+++ sale_stock_migration/sale.py
@@ -14,27 +14,29 @@ class SaleOrder(orm.Model):
     _inherit = 'sale.order'
 
     _columns = {
-        'create_date': fields.datetime(
-            'Create date'),
+        #'create_date': fields.datetime(
+        #    'Create date'),
+        'no_picking': fields.boolean('No Picking'),
     }
 
     def _create_pickings_and_procurements(self, cr, uid, order, order_lines,
                                           picking_id=False, context=None):
         #import pdb;pdb.set_trace()
-        if not order.company_id.start_picking_create \
-                or order.create_date > order.company_id.start_picking_create:
+        #if not order.company_id.start_picking_create \
+        #        or order.create_date > order.company_id.start_picking_create:
+        if not order.no_picking:
             super(SaleOrder, self)._create_pickings_and_procurements(
                 cr, uid, order, order_lines, picking_id=picking_id, context=context)
         return True
 
 
-class ResCompany(orm.Model):
-    _inherit = 'res.company'
-
-    _columns = {
-        'start_picking_create': fields.datetime(
-            'Picking creation',
-            help="Before this time, sales order will not create stock pickings."
-                 " No view to modify this date, changes are directly "
-                 "done by admin user in database."),
-    }
+#class ResCompany(orm.Model):
+#    _inherit = 'res.company'
+#
+#    _columns = {
+#        'start_picking_create': fields.datetime(
+#            'Picking creation',
+#            help="Before this time, sales order will not create stock pickings."
+#                 " No view to modify this date, changes are directly "
+#                 "done by admin user in database."),
+#    }
